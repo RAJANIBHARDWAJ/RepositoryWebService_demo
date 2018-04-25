d041b521a312f390cc98242183fab56ce7023d1c
RAJANIBHARDWAJ <37225139+RAJANIBHARDWAJ@users.noreply.github.com>
2018-04-25 15:23:09 +0530

    change in author class

--- WebService_Demo/src/com/book/Author.java
+++ WebService_Demo/src/com/book/Author.java
@@ -4,6 +4,8 @@ public class Author {
 
 	public static void main(String[] args) {
 		System.out.println("hello im in develops R1 branch");
+		
+		System.out.println("modified in Authors Class");
 
 	}
 
